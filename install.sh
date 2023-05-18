632f233b12c4136bf81260813b43c78ff2bd9ac6
highttechharmony <smcgrath@nfatech.com>
2023-05-18 10:37:51 +0100

    Suppresses superfluous errors to console

--- install.sh
+++ install.sh
@@ -4,5 +4,5 @@ sudo cp perfektday.service /lib/systemd/system/perfektday.service
 sudo systemctl daemon-reload
 sudo systemctl enable perfektday.service
 sudo systemctl start perfektday.service
-rm pdc_data.json
+rm pdc_data.json > /dev/null
 
